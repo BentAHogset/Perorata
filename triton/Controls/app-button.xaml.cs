036bb38013e7ea6baa8bd616cad47c575a765e8c
Bent-Arne Høgset <bent-arne.hogset@visma.com>
2021-02-01 17:02:21 +0100

    app button

---
+++ triton/Controls/app-button.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace triton.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class app_button : ContentView
+    {
+        public app_button()
+        {
+            InitializeComponent();
+            lblButton.SetBinding(Label.TextProperty, new Binding("LabelText", source: this));
+        }
+
+        public static readonly BindableProperty LabelTextProperty = BindableProperty.Create(
+           nameof(LabelText),
+           typeof(string),
+           typeof(Label),
+           string.Empty);
+
+        public string LabelText
+        {
+            get
+            {
+
+                return (string)GetValue(LabelTextProperty);
+            }
+            set
+            {
+                SetValue(LabelTextProperty, value);
+            }
+        }
+
+    }
+}
