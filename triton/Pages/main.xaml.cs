a65a31d0b2e059e5919b5e13c23ba479e394e486
Bent-Arne Høgset <bent-arne.hogset@visma.com>
2021-02-03 10:29:42 +0100

    main page refactor

--- triton/Pages/main.xaml.cs
+++ triton/Pages/main.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +12,17 @@ namespace triton.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class main : ContentPage
     {
+        public ICommand ClickVacation;
+
         public main()
         {
             InitializeComponent();
+            
+
+            ClickVacation = new Command(() =>
+            {
+                Navigation.PushAsync(new vacation());
+            });
         }
 
         private void Vacation_Clicked(object sender, EventArgs e)
@@ -22,5 +30,6 @@ namespace triton.Pages
 
             Navigation.PushAsync(new vacation());
         }
+        
     }
 }
