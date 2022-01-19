14353895b87e2479aa0e7258c6569f9066e17997
Bent-Arne Høgset <bent-arne.hogset@visma.com>
2022-01-19 15:53:32 +0100

    testing dropdown

--- triton/Pages/vacation.xaml.cs
+++ triton/Pages/vacation.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using triton.Controls;
 using triton.Models;
 using triton.Providers;
 using triton.Services;
@@ -17,12 +18,44 @@ namespace triton.Pages
     public partial class vacation : ContentPage
     {
 
+        public List<string> Items1 = new List<string>();
+        public List<string> Items2 = new List<string>();
+        public bool IsItem1 = true;
+
         public vacation()
         {
             InitializeComponent();
 
             var vacationProvider = App.Kernel.Get<IVacationProvider>();
             BindingContext = new VacationViewModel(vacationProvider);
+
+
+            for (int i = 0; i < 4; i++)
+            {
+                Items1.Add(i.ToString());
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Items2.Add(i.ToString());
+            }
+
+            DropDown.ItemsSource = Items1;
+            DropDown.SelectedIndex = 1;
+            DropDown.ItemSelected += OnDropdownSelected;
+        }
+
+
+        private void OnDropdownSelected(object sender, DropDown.ItemSelectedEventArgs e)
+        {
+            //label.Text = IsItem1 ? Items1[e.SelectedIndex] : Items2[e.SelectedIndex];
+        }
+
+        private void btn_Clicked(object sender, EventArgs e)
+        {
+            DropDown.ItemsSource = IsItem1 ? Items2 : Items1;
+            DropDown.SelectedIndex = IsItem1 ? 5 : 1;
+            IsItem1 = !IsItem1;
         }
     }
 }
