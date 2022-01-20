beb3f40399602a2b7b8ff0c97e8090425d5e6880
Bent-Arne Høgset <bent-arne.hogset@visma.com>
2022-01-20 10:19:06 +0100

    adding custom entry

---
+++ triton.Android/Controls/EmptyEntryRenderer.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.Graphics.Drawables;
+using triton.Controls;
+using triton.Droid.Controls;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(EmptyEntry), typeof(EmptyEntryRenderer))]
+namespace triton.Droid.Controls
+{
+    public class EmptyEntryRenderer : EntryRenderer
+    {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="context"></param>
+        public EmptyEntryRenderer(Context context) : base(context)
+        {
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                GradientDrawable gd = new GradientDrawable();
+                gd.SetColor(global::Android.Graphics.Color.Transparent);
+                this.Control.SetBackground(gd);
+                Control.SetTextColor(Element.TextColor.ToAndroid());
+            }
+        }
+    }
+}
---
+++ triton.iOS/Controls/EmptyEntryRenderer.cs
@@ -0,0 +1,27 @@
+﻿using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using triton.Controls;
+using triton.iOS.Controls;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(EmptyEntry), typeof(EmptyEntryRenderer))]
+namespace triton.iOS.Controls
+{
+    public class EmptyEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null && e.NewElement != null)
+            {
+                Control.BorderStyle = UITextBorderStyle.None;
+                Control.TextColor = Element.TextColor.ToUIColor();
+            }
+        }
+    }
+}
